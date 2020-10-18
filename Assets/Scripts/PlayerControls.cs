c62338a2dc6c9d5b3c70f8f06255f9f65583b516
ms-heisenbug <j.piorkowska94@gmail.com>
2020-10-19 00:30:04 +0200

    added idle, run, jump, double jump animations and handlers

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,212 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""Move"",
+            ""id"": ""4d729629-5156-4444-b6cc-2909ff03c3cc"",
+            ""actions"": [
+                {
+                    ""name"": ""Sideways"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""434ba5f4-1294-4768-a00c-40bafe494ab8"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""8b1d6ca8-7e61-4bdc-8883-59cc0a286d0f"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""DoubleJump"",
+                    ""type"": ""Button"",
+                    ""id"": ""ae1fd5b5-07f2-4d50-ac3e-b059484f3b07"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""LeftRight"",
+                    ""id"": ""0fcc2080-5212-47e8-bb52-bf2e701830f7"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Sideways"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""b76b2fb6-b66c-483d-99f4-f90994438341"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Sideways"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""f1d18473-7a3c-4126-9c83-7ea5479a04e6"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Sideways"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""85385450-ad56-4461-8dd0-5153dc853686"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""65648e9d-4b49-4399-b9a6-347065b786c3"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": ""MultiTap"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""DoubleJump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Move
+        m_Move = asset.FindActionMap("Move", throwIfNotFound: true);
+        m_Move_Sideways = m_Move.FindAction("Sideways", throwIfNotFound: true);
+        m_Move_Jump = m_Move.FindAction("Jump", throwIfNotFound: true);
+        m_Move_DoubleJump = m_Move.FindAction("DoubleJump", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Move
+    private readonly InputActionMap m_Move;
+    private IMoveActions m_MoveActionsCallbackInterface;
+    private readonly InputAction m_Move_Sideways;
+    private readonly InputAction m_Move_Jump;
+    private readonly InputAction m_Move_DoubleJump;
+    public struct MoveActions
+    {
+        private @PlayerControls m_Wrapper;
+        public MoveActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Sideways => m_Wrapper.m_Move_Sideways;
+        public InputAction @Jump => m_Wrapper.m_Move_Jump;
+        public InputAction @DoubleJump => m_Wrapper.m_Move_DoubleJump;
+        public InputActionMap Get() { return m_Wrapper.m_Move; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(MoveActions set) { return set.Get(); }
+        public void SetCallbacks(IMoveActions instance)
+        {
+            if (m_Wrapper.m_MoveActionsCallbackInterface != null)
+            {
+                @Sideways.started -= m_Wrapper.m_MoveActionsCallbackInterface.OnSideways;
+                @Sideways.performed -= m_Wrapper.m_MoveActionsCallbackInterface.OnSideways;
+                @Sideways.canceled -= m_Wrapper.m_MoveActionsCallbackInterface.OnSideways;
+                @Jump.started -= m_Wrapper.m_MoveActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_MoveActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_MoveActionsCallbackInterface.OnJump;
+                @DoubleJump.started -= m_Wrapper.m_MoveActionsCallbackInterface.OnDoubleJump;
+                @DoubleJump.performed -= m_Wrapper.m_MoveActionsCallbackInterface.OnDoubleJump;
+                @DoubleJump.canceled -= m_Wrapper.m_MoveActionsCallbackInterface.OnDoubleJump;
+            }
+            m_Wrapper.m_MoveActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Sideways.started += instance.OnSideways;
+                @Sideways.performed += instance.OnSideways;
+                @Sideways.canceled += instance.OnSideways;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @DoubleJump.started += instance.OnDoubleJump;
+                @DoubleJump.performed += instance.OnDoubleJump;
+                @DoubleJump.canceled += instance.OnDoubleJump;
+            }
+        }
+    }
+    public MoveActions @Move => new MoveActions(this);
+    public interface IMoveActions
+    {
+        void OnSideways(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnDoubleJump(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Win32;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    private PlayerControls controls;
+    [SerializeField] float speed;
+    private Rigidbody2D rb;
+    private bool facingRight;
+    private Animator animator;
+    
+    [SerializeField] private float jumpForce;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private Transform topLeftCorner;
+    [SerializeField] private Transform bottomRightCorner;
+    private bool doubleJumping;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+        rb = GetComponent<Rigidbody2D>();
+        facingRight = true;
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+    }
+
+    private void Update()
+    {
+        int jumpState = animator.GetInteger("JumpState");
+
+        if (rb.velocity.y < 0 && jumpState > 0)
+        {
+            //fall
+            animator.SetInteger("JumpState", 3);
+        }
+
+        if (controls.Move.Jump.triggered || controls.Move.DoubleJump.triggered)
+        {
+            Jump();
+            Debug.Log("Jump pressed");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveLeftRightInput = controls.Move.Sideways.ReadValue<float>() * speed * Time.deltaTime;
+        animator.SetFloat("Speed", Mathf.Abs(moveLeftRightInput));
+
+        if((moveLeftRightInput < 0 && facingRight) || (moveLeftRightInput > 0 && !facingRight))
+        {
+            Flip();
+        }
+     
+        Vector3 currentPos = transform.position;
+        currentPos.x += moveLeftRightInput;
+        transform.position = currentPos;
+    }
+
+    private void Jump()
+    {
+        if (doubleJumping)
+        {
+            return;
+        }
+
+        Vector2 velocity = rb.velocity;
+        velocity.y = jumpForce;
+        rb.velocity = velocity;
+
+        int jumpState = animator.GetInteger("JumpState");
+        Debug.Log($"Jump() -> jumpState = {jumpState}");
+
+        if (jumpState == 0 || jumpState == 1)
+        {
+            if (jumpState == 1)
+            {
+                doubleJumping = true;
+            }
+
+            animator.SetInteger("JumpState", ++jumpState);          //hehehehehehe jumpState++ hehehehehe
+        }
+        else if(jumpState == 3)
+        {
+            doubleJumping = false;
+        }
+
+        Debug.Log("force added");
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(Vector3.up * 180);
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        animator.SetInteger("JumpState", 0);
+        doubleJumping = false;
+    }
+}
